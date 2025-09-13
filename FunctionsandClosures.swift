8044fe57edbefa1f25291287a3baef266a8679be
Lindsay Hall <143295788+lindsayh17@users.noreply.github.com>
2025-09-12 22:30:11 -0400

    Exercise 2
    Not working quite right

--- FunctionsandClosures.swift
+++ FunctionsandClosures.swift
@@ -36,3 +36,23 @@ func isMultiple(of testValues: Set<Int>, in numbers: Set<Int>) -> (yes: Set<Int>
 }
 
 print(isMultiple(of: Set([3, 7]), in: Set([-12, -7, -42, 0, 2, 3, 7, 105, 19])))
+
+
+// Categorizing Integers II
+
+func categorize(_ numbers: Set<Int>, using check: (Int) -> Bool) -> (yes: Set<Int>, no: Set<Int>){
+    var yes = Set<Int>()
+    var no = Set<Int>()
+    
+    for n in numbers{
+        if check(n){
+            yes.insert(n)
+        }else{
+            no.insert(n)
+        }
+    }
+    
+    return(yes, no)
+}
+
+print(categorize(Set([-5, 0, 1, 5, 6]), using: {i in i > 0})) //TODO: check (printing weird)
